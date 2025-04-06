d8e43d627c441154ad2595aebbbbd2b5c951b335
Sebastian Chitiva <manuchitivar@gmail.com>
2025-04-06 12:45:06 -0500

    oculto la web

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -155,13 +155,6 @@ const Header = () => {
             <ThemeToggler />
 
             <Link
-              href="/auth/signin"
-              className="text-regular font-medium text-waterloo hover:text-primary"
-            >
-              Login
-            </Link>
-
-            <Link
               href="/auth/signup"
               className="flex items-center justify-center rounded-full bg-primary px-7.5 py-2.5 text-regular text-white duration-300 ease-in-out hover:bg-primaryho"
             >
